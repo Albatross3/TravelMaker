287e7590ed8fdc31ab7c05942ba4f693181c1871
Albatross3 <superstring77@gmail.com>
2023-09-26 22:52:09 +0900

    refactor: redis 필요없는 클래스 제거
    - redis config 파일 이외 제외

--- src/main/java/shop/zip/travel/global/config/RedisConfig.java
+++ src/main/java/shop/zip/travel/global/config/RedisConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -22,12 +20,4 @@ public class RedisConfig {
     return new LettuceConnectionFactory(host, port);
   }
 
-  @Bean
-  public RedisTemplate<String, String> redisTemplate() {
-    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-    redisTemplate.setConnectionFactory(redisConnectionFactory());
-    redisTemplate.setKeySerializer(new StringRedisSerializer());
-    redisTemplate.setValueSerializer(new StringRedisSerializer());
-    return redisTemplate;
-  }
 }
